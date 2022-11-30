b17908045195e15f9333e7f8ce04feb043f65c3c
NotALakeLurk <TheRedCoat787@Gmail.com>
2022-11-29 22:54:48 -0500

    1.0.0 Release
    Allows for generation of JSFck from the full English alphabet, along with misc symbols.

--- Utilites.js
+++ Utilites.js
@@ -4,20 +4,23 @@ const f_true = "!![]";
 const f_NaN = "(+[![]])";
 const f_undefined = "[][[]]"
 
+const f_zero = "(+[])"
+const f_one = "(+!+[])"
+
 
 let map = {};
 
 // digits
-map[0] = `(${number(0)})`;
-map[1] = `(${number(1)})`;
-map[2] = `(${number(2)})`;
-map[3] = `(${number(3)})`;
-map[4] = `(${number(4)})`;
-map[5] = `(${number(5)})`;
-map[6] = `(${number(6)})`;
-map[7] = `(${number(7)})`;
-map[8] = `(${number(8)})`;
-map[9] = `(${number(9)})`;
+map[0] = `(${f_zero}+[])`;
+map[1] = `(${f_one}+[])`;
+map[2] = `(${f_one}+${f_one}+[])`;
+map[3] = `(${f_one}+${f_one}+${f_one}+[])`;
+map[4] = `(${f_one}+${f_one}+${f_one}+${f_one}+[])`;
+map[5] = `(${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+[])`;
+map[6] = `(${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+[])`;
+map[7] = `(${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+[])`;
+map[8] = `(${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+ [])`;
+map[9] = `(${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+${f_one}+[])`;
 
 // false
 map.f = `(${stringify(f_false)}[${number(0)}])`;
@@ -40,7 +43,7 @@ map.n = `(${stringify(f_undefined)}[${number(1)}])`;
 map.d = `(${stringify(f_undefined)}[${number(2)}])`;
 map.i = `(${stringify(f_undefined)}[${number(5)}])`;
 
-const f_Infinity = `(+(${number(1)}+${map.e}+${numString(1000)}))`;
+const f_Infinity = `(+(${makeString(1)}+${makeString("e")}+${makeString(1000)}))`;
 
 // Infinity
 map.I = `(${stringify(f_Infinity)}[${number(0)}])`;
@@ -55,17 +58,17 @@ map.o = `([][${f_filter}]+[])[${number(6)}]`;
 map[" "] = `([][${f_filter}]+[])[${number(8)}]`;
 map["("] = `([][${f_filter}]+[])[${number(15)}]`;
 map[")"] = `([][${f_filter}]+[])[${number(16)}]`;
-map["{"] = `([][${f_filter}]+[])[${number(16)}]`;
-map["["] = `([][${f_filter}]+[])[${number(18)}]`;
-map.v = `([][${f_filter}]+[])[${number(23)}]`;
-map["]"] = `([][${f_filter}]+[])[${number(30)}]`;
-map["}"] = `([][${f_filter}]+[])[${number(32)}]`;
+map["{"] = `([][${f_filter}]+[])[${number(18)}]`;
+map["["] = `([][${f_filter}]+[])[${number(20)}]`;
+map.v = `([][${f_filter}]+[])[${number(25)}]`;
+map["]"] = `([][${f_filter}]+[])[${number(32)}]`;
+map["}"] = `([][${f_filter}]+[])[${number(34)}]`;
 
 const f_constructor = makeString("constructor");
 
 // 1.1e+20
-map["."] = `((+(${numString(11)}+${map.e}+${numString(20)})+[])[${number(1)}])`;
-map["+"] = `((+(${numString(11)}+${map.e}+${numString(20)})+[])[${number(4)}])`;
+map["."] = `((+(${makeString(11)}+${makeString("e")}+${makeString(20)})+[])[${number(1)}])`;
+map["+"] = `((+(${makeString(11)}+${makeString("e")}+${makeString(20)})+[])[${number(4)}])`;
 
 // String
 map.S = `((([]+[])[${makeString("constructor")}]+[])[${number(9)}])`;
@@ -78,61 +81,64 @@ map["\""] = `((([]+[])[${makeString("fontcolor")}]()+[])[${number(12)}])`;
 map[">"] = `((([]+[])[${makeString("fontcolor")}]()+[])[${number(23)}])`;
 map["/"] = `((([]+[])[${makeString("fontcolor")}]()+[])[${number(26)}])`;
 
-function number(n) {
-  let res = `+[]`;
-  for (let i = 0; i < n; i++) {
-    res += "+" + `(+!+[])`;
-  }
-  return res;
+// map[";"] = `;`
+
+function number(n) { // Might be best to just replace all occurances with `(+${makeString(n)})`
+  return `(+(${makeString(n)}))`
 }
 
 function stringify(v) { return `(${v}+[])` }
 // function numberify(n) { return `(+(${n}))` }
 
-function numString(n) {
-  n = n.toString().split(([]+[]));
+// function numString(n) {
+//   n = n.toString().split('');
 
-  n.forEach((c, i) => {
-    n[i] = `[${number(+c)}]`;
-  });
+//   n.forEach((c, i) => {
+//     n[i] = `(${map[c]})`;
+//   });
 
-  return n.join("+");
-}
+//   return n.join("+");
+// }
 
-function makeString(s) {
-  s = s.toString().split(([]+[]));
-  s2 = [];
+function makeString(str) {
+  str = str.toString();
+  let res = [];
 
-  for (var c of s) {
-    if (c in map) { s2.push(map[c]); } else { throw(`: \`${c}\` does not exist in \`map\``); }
+  for (var c of str) {
+    let mapped;
+
+    try {
+      if (!(c in map)) { throw (`: \`${c}\` does not exist in \`map\``); } // Eventually this should be blacklisted
+
+      mapped = map[c];
+      mapVal = eval(mapped);
+      if (mapVal != c) { throw (`: \`${c}\` is incorrect in \`map\`; mapVal = \`${mapVal}\``); }
+
+    } catch (error) { mapped = any_letter(c); }
+    
+    res.push(mapped);
   }
 
-  return s2.join("+");
+  return res.join("+");
 }
 
 function f_function(func) {
-  func = makeString(func.toString());
+  func = makeString(func);
 
-  str = `[][${f_filter}][${f_constructor}](${func})()`; // TODO - func is the problem 
+  str = `[][${f_filter}][${f_constructor}](${func})()`;
 
   return str;
 }
 
-// data = makeString('console.log("a")');
-data = f_function('console.log("lol git gud 1337 too cool 4 u")');
+const base36 = "0123456789abcdefghijklmnopqrstuvwxyz"; // `toString()` allows for converting to alternate bases, base 36 includes every letter
+function any_letter(letter) {
+  if (!base36.includes(letter)) { throw(`: \`${letter}\` not in base36`); }
 
-const fs = require('fs')
-fs.writeFileSync("output.js", data)
+  i = base36.indexOf(letter);
 
-console.log(data);
-eval(data);
+  res = `(${number(i)})[${makeString("toString")}](${number(36)})`;
 
-// "
-// ([]+[])[(![]+[])[+[]]+(!![]+[][(![]+[])[+[]]+(![]+[])[!+[]+!+[]]+(![]+[])[+!+[]]+(!![]+[])[+[]]])[+!+[]+[+[]]]+([][[]]+[])[+!+[]]+(!![]+[])[+[]]+([][(![]+[])[+[]]+(![]+[])[!+[]+!+[]]+(![]+[])[+!+[]]+(!![]+[])[+[]]]+[])[!+[]+!+[]+!+[]]+(!![]+[][(![]+[])[+[]]+(![]+[])[!+[]+!+[]]+(![]+[])[+!+[]]+(!![]+[])[+[]]])[+!+[]+[+[]]]+(![]+[])[!+[]+!+[]]+(!![]+[][(![]+[])[+[]]+(![]+[])[!+[]+!+[]]+(![]+[])[+!+[]]+(!![]+[])[+[]]])[+!+[]+[+[]]]+(!![]+[])[+!+[]]]()[+!+[]+[!+[]+!+[]]]
+  return res;
+}
 
 ;
-
-/*
-  CURRENT PROBLEM: some dumb string issue
-*/ 
-
